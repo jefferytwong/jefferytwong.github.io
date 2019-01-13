6f1f75668da7749e5db237b85ee19ce88e73fb99
Jerry <jerrylee1697@gmail.com>
2019-01-13 06:48:10 -0800

    sql

--- send_post.php
+++ send_post.php
@@ -3,5 +3,5 @@
 $connect = mysqli_connect("35.236.23.230","root","","businesses");
 //Sending form data to sql db.
 mysqli_query($connect,"INSERT INTO data (name, phone, email, EId, cal, menu)
-VALUES ('$_POST[name]', '$_POST[phone]', '$_POST[email]', '$_POST[EID]', 'None', '$_POST[menu]')";
+VALUES ('$_POST[name_form]', '$_POST[phone_form]', '$_POST[email_form]', '$_POST[EID_form]', 'None', '$_POST[menu_form]')";
 ?>
