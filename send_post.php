2e1db5946f462cd11e7aadaccf43a18e86851303
Jerry <jerrylee1697@gmail.com>
2019-01-13 06:52:54 -0800

    Testing PHP prints

--- send_post.php
+++ send_post.php
@@ -1,6 +1,8 @@
 <?php
+echo '<script>console.log(name_form)</script>';
 //Connecting to sql db.
 $connect = mysqli_connect("35.236.23.230","root","","businesses");
+echo '<script>console.log(phone_form)</script>';
 //Sending form data to sql db.
 mysqli_query($connect,"INSERT INTO data (name, phone, email, EId, cal, menu)
 VALUES ('$_POST[name_form]', '$_POST[phone_form]', '$_POST[email_form]', '$_POST[EID_form]', 'None', '$_POST[menu_form]')";
