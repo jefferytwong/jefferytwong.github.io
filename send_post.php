f2ea2ce40f5c724073b2bb236951ad24a2b95c4a
Jerry <jerrylee1697@gmail.com>
2019-01-13 07:09:17 -0800

    redo

--- send_post.php
+++ send_post.php
@@ -1,9 +1,9 @@
 <?php
 echo $_POST["name_form"];
 //Connecting to sql db.
-// $connect = mysqli_connect("35.236.23.230","root","","businesses");
+$connect = mysqli_connect("35.236.23.230","root","","businesses");
 echo $_POST["phone_form"];
 //Sending form data to sql db.
-// mysqli_query($connect,"INSERT INTO data (name, phone, email, EId, cal, menu)
-// VALUES ('$_POST["name_form"]', '$_POST[phone_form]', '$_POST[email_form]', '$_POST[EID_form]', NULL, '$_POST[menu_form]')";
+mysqli_query($connect,"INSERT INTO data (name, phone, email, EId, cal, menu)
+VALUES ('$_POST["name_form"]', '$_POST[phone_form]', '$_POST[email_form]', '$_POST[EID_form]', NULL, '$_POST[menu_form]')";
 ?>
